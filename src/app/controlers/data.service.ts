8b0206513b62cb0403b6a838e191a81d8ed1f151
Moshe Peres <mosheper99@gmail.com>
2019-07-04 16:39:54 -0400

    added features to recipies

--- src/app/controlers/data.service.ts
+++ src/app/controlers/data.service.ts
@@ -1,16 +1,16 @@
 import { Injectable } from '@angular/core';
-import {Observable} from "rxjs";
-import {HttpClient} from "@angular/common/http";
+import {Observable} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
-  baseUrl = "https://api.edamam.com/search?q=";
+  baseUrl = 'https://api.edamam.com/search?q=';
 
   constructor(private http: HttpClient) { }
 
   getRecipies(search: string): Observable<any> {
-    return this.http.get(this.baseUrl + search + "&app_id=e04a40f7&app_key=a9558ae3efc86ed9c55e74631ea574b8");
+    return this.http.get(this.baseUrl + search + '&app_id=e04a40f7&app_key=a9558ae3efc86ed9c55e74631ea574b8');
   }
 }
--- src/app/views/search-results-recipies/search-results-recipies.component.ts
+++ src/app/views/search-results-recipies/search-results-recipies.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
-import {DataService} from "../../controlers/data.service";
+import {DataService} from '../../controlers/data.service';
 
 @Component({
   selector: 'app-search-results-recipies',
   templateUrl: './search-results-recipies.component.html',
-  styleUrls: ['./search-results-recipies.component.sass']
+  styleUrls: ['./search-results-recipies.component.scss']
 })
 export class SearchResultsRecipiesComponent implements OnInit {
   search: string;
