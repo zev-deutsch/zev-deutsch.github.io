610ec013c707a64001e6fefa97d2005f843439ae
Moshe Peres <mosheper99@gmail.com>
2019-07-04 15:13:47 -0400

    added the class

--- src/app/controlers/data.service.ts
+++ src/app/controlers/data.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
 })
 export class DataService {
   search: string;
-  baseUrl = "https://api.edamam.com/search?q=" + this.search + "&app_id=e04a40f7&app_key=a9558ae3efc86ed9c55e74631ea574b8";
+  baseUrl = 'https://api.edamam.com/search?q=' + this.search + '&app_id=e04a40f7&app_key=a9558ae3efc86ed9c55e74631ea574b8';
 
   constructor() { }
 }
